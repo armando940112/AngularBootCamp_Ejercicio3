c4ee617415c5093d1fe8a85fbce9acad1e742d4e
ALondono <armando.londono@yuxipacific.com>
2018-02-10 10:31:43 -0500

    Changes to complete the execise#3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor() {}
-  ngOnInit(){}
+export class AppComponent implements OnInit{
+  message: string;
 
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit() {
+    this.probar();
+  }
+
+  probar(): void {
+    setInterval(() =>
+      this.httpClient.get<any>("https://api.icndb.com/jokes/random").subscribe(x => this.message = x.value.joke)
+      , 5000
+    );
+  }
 }
